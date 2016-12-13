065e95fb3eab2b6f71c4d580acad5c03ef633679
krishna <krishna981996@gmail.com>
2016-12-13 21:14:08 +0530

    webpage updated

--- templates.py
+++ templates.py
@@ -1,10 +1,26 @@
 import webapp2
 import jinja2
 import os
+import db
+from user import *
+
+staff_list = [{"user":"muttan", "pass":"ina103"},{"user":"meenakshi", "pass":"mcp23008"}]
+def db_init:
+	for staff in staff_list:
+		username = staff["user"]
+		password = staff["pass"]
+		hash_pw = make_pw_hash(username, password)
+		a = users(username = username, password = hash_pw)
+		a.put()
+
+class users(db.Model):
+	username = db.StringProperty(required = True)
+	password = db.StringProperty(required = True)
+	created = db.DateTimeProperty(auto_now_add = True)
+
 
 template_dir = os.path.join(os.path.dirname(__file__),'templates')
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),autoescape=True)
-
 
 class Handler(webapp2.RequestHandler):
 	def write(self,*a,**kw):
@@ -71,8 +87,34 @@ class forumHandler(Handler):
 	def get(self):
 		self.render("forum.html")
 
+class researchHandler(Handler):
+	def get(self):
+		self.render("research.html")
+
+class staffLoginHandler(Handler):
+	def render_front(self,username="",password=""):
+		self.render("staff_login.html",username = username, password = password)
+	def get(self):
+		self.render_front()
+	def post(self):
+		username = self.request.get("username")
+		password = self.request.get("password")
+		user_name = valid_username(username)
+		pass_word = valid_password(password)
+		if user_name and pass_word:
+			a = db.GqlQuery("SELECT * FROM users WHERE username='%s'"%username)
+			user = a.get()
+			if user:
+				valid = valid_pw(username, password, user.password)
+				if valid:
+					id = user.key().id()
+					cookie_user_id = make_secure_val(str(id))
+					self.response.headers.add_header('Set-Cookie', 'user_id=%s; Path=/'%cookie_user_id)
+					self.redirect('/')
+
 app = webapp2.WSGIApplication([
-    ('/', MainPage),('/infra',infraHandler),('/ug',UGHandler,),('/pg',PGHandler),('/part',partHandler),
+    ('/', MainPage),('/infra',infraHandler),('/ug',UGHandler,),('/pg',PGHandler),('/part',partHandler),('/research',researchHandler),
     ('/staff',staffHandler),('/nstaff',nStaffHandler),('/students',studentsHandler),('/workshop',workshopHandler),
-    ('/fv',fvHandler),('/ecea',eceaHandler),('/placements',placementsHandler),('/contact',contactHandler),('/forum',forumHandler)
+    ('/fv',fvHandler),('/ecea',eceaHandler),('/placements',placementsHandler),('/contact',contactHandler),('/forum',forumHandler),
+    ('staff-signup',staffSignupHandler),('/staff-login',staffLoginHandler)
 ], debug=True)
---
+++ user.py
@@ -0,0 +1,37 @@
+import re
+import hmac
+import random
+import hashlib
+
+USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
+PASS_RE = re.compile(r"^.{3,20}$")
+EMAIL_RE = re.compile(r"^[\S]+@[\S]+.[\S]+$")
+
+def valid_username(username):
+	return USER_RE.match(username)
+
+def valid_password(password):
+	return PASS_RE.match(password)
+
+def hash_str(s):
+	return hmac.new(secret,s).hexdigest()
+
+def make_secure_val(s):
+	return '%s|%s' %(s,hash_str(s))
+
+def check_secure_val(h):
+	val = h.split('|')[0]
+	if h == make_secure_val(val):
+		return val
+def make_salt():
+	return ''.join(random.choice(string.letters) for x in range(5))
+
+def make_pw_hash(name, pw, salt = None):
+	if not salt:
+		salt = make_salt()
+	h = hashlib.sha256(name + pw + salt).hexdigest()
+	return '%s,%s' % (h,salt)
+
+def valid_pw(name, pw, h):
+	salt = h.split(',')[1]
+	return h == make_pw_hash(name, pw, salt)
